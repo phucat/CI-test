200abdd42fe439d90aace184efb60986a306f763
richmond gozarin <ralph.gozarin@cloudsherpas.com>
2015-03-11 15:17:46 +0800

    Enable prime_caches()

--- app/controllers/calendars.py
+++ app/controllers/calendars.py
@@ -332,7 +332,7 @@ class Calendars(Controller):
 
     @route_with(template='/api/user_removals/deleting/users')
     def api_deleting_users(self):
-        # google_directory.prime_caches()
+        google_directory.prime_caches()
         deleted_users = google_directory.get_all_deleted_users()
         # users_email = google_directory.get_all_users_cached()
 
