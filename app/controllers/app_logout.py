0b11064918a40d1741ecfb56b15b7da51db84074
Andrew Fleming <andrew.fleming@cloudsherpas.com>
2015-04-02 17:37:54 -0400

    Logout button implemented

--- app/controllers/app_logout.py
+++ app/controllers/app_logout.py
@@ -1,26 +1,12 @@
 from ferris import Controller, route_with
-import Cookie
+from google.appengine.api import users
 
 
-
-class AppLogut(Controller):
+class AppLogout(Controller):
+    class Meta:
+        prefixes = ('api',)
 
     @route_with(template='/api/app_logout', methods=['GET'])
     def api_app_logout(self):
-    # On the production instance, we just remove the session cookie, because
-        # redirecting users.create_logout_url(...) would log out of all Google
-        # (e.g. Gmail, Google Calendar).
-        #
-        # It seems that AppEngine is setting the ACSID cookie for http:// ,
-        # and the SACSID cookie for https:// . We just unset both below.
-        cookie = Cookie.SimpleCookie()
-        cookie['ACSID'] = ''
-        cookie['ACSID']['expires'] = -86400  # In the past, a day ago.
-        self.response.headers.add_header(*cookie.output().split(': ', 1))
-        cookie = Cookie.SimpleCookie()
-        cookie['SACSID'] = ''
-        cookie['SACSID']['expires'] = -86400
-        self.response.headers.add_header(*cookie.output().split(': ', 1))
-        self.redirect('/')
-
-        return
+        logoutURL = users.create_logout_url('/')
+        return self.redirect(logoutURL)
